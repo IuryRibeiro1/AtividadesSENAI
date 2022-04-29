5274b1356efaf72f49fbd8f338d2203a51d32641
Iury Ribeiro <iury211@gmail.com>
2022-04-29 11:59:30 -0300

    Atividade 7 SA2

--- src/com/company/Conexao.java
+++ src/com/company/Conexao.java
@@ -16,10 +16,15 @@ public class Conexao {
         try {
             connection = DriverManager.getConnection(DATABASE_URL, "root", "");
             stm = connection.createStatement();
+            //Este comando permite alterar tabela criando uma coluna no banco de dados.
             stm.executeUpdate("alter table produtos add column Validade varchar(30)");
+            //Este comando insere dados na tabela selecionado (nesse caso aqui a tabela ''produtos'' recebe novos valores.
             stm.executeUpdate("insert into produtos values ( 6, 'Biscoito' , 'R$ 2.99', '5 Unidades'  , 'Valido até 28-04-2025') ");
+            //Este comando permite alterar tabela deletando uma coluna no banco dados.
             stm.executeUpdate("alter table produtos drop column Validade");
+            //Comando de manipulação de linhas, deletando a linha '5' selecionada da tabela 'Produtos'
             stm.executeUpdate("delete from produtos where idprodutos = '5'");
+            //Comando que executa a consulta de todos os dados dentro da tabela 'produtos'.
             rs = stm.executeQuery("select * from produtos");
 
             while (rs.next())
